0675cc334865f78a2195d6087e73da311b18f254
Syed Haseeb Shah <codingsheep17@gmail.com>
2026-08-21 14:32:48 +0500

    Working with contrast

---
+++ src/postprocessing/contrast.py
@@ -0,0 +1,17 @@
+import cv2
+
+def enhance_contrast(image):
+    lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+
+    l_channel, a_channel, b_channel = cv2.split(lab)
+
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+
+    enhanced_l = clahe.apply(l_channel)
+
+    enhanced_lab = cv2.merge((enhanced_l, a_channel, b_channel))
+
+    enhanced_image = cv2.cvtColor(enhanced_lab, cv2.COLOR_LAB2BGR)
+
+    return enhanced_image
+
