239b11fc11f2e2ad87a3f5c3869d6efbb780cfe8
Syed Haseeb Shah <codingsheep17@gmail.com>
2026-08-21 14:18:14 +0500

    Tested the denoising

--- src/postprocessing/desnoising.py
+++ src/postprocessing/desnoising.py
@@ -12,3 +12,14 @@ def denoise_image(image):
 
     return denoised
 
+if __name__ == "__main__":
+    image = cv2.imread("D:/Desktop/AI IMG ENHANCER/real_esrgan_output.png")
+
+    if image is None:
+        raise ValueError("Unable to load image.")
+
+    denoised = denoise_image(image)
+
+    cv2.imwrite("denoised_test.png", denoised)
+
+    print("Denoising completed")
